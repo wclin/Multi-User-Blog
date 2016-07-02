0bf0009321aadf1c7bc0a597bd9176670089b3c6
wclin <r03922092+git@csie.ntu.edu.tw>
2016-07-02 10:09:29 +0800

    Merge hw3

--- blog.py
+++ blog.py
@@ -1,19 +1,4 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import os
 import jinja2
 import webapp2
@@ -25,44 +10,62 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 	extensions=['jinja2.ext.autoescape'],
 	autoescape=True)
 
-class Author(db.Model):
-    name = db.StringProperty(required = True)
-
-class Post(db.Model):
-    #author = db.ReferenceProperty(Author)
-    author = db.StringProperty()
-    title = db.StringProperty()
-    content = db.TextProperty()
-    created = db.DateTimeProperty(auto_now_add = True)
+def render_str(template, **params):
+	t = JINJA_ENVIRONMENT.get_template(template)
+	return t.render(params)
 
 class Handler(webapp2.RequestHandler):
-    def write(self, *a, **kw):
-	self.response.out.write(*a, **kw)
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
 
-    def render_str(self, template, **params):
-	return (JINJA_ENVIRONMENT.get_template(template)).render(params)
+	def render_str(self, template, **params):
+		return (JINJA_ENVIRONMENT.get_template(template)).render(params)
 
-    def render(self, template, **kw):
-	self.write(self.render_str(template, **kw))
-    
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
+
+#class Author(db.Model):
+#	name = db.StringProperty(required = True)
+
+def blog_key(name = 'default'):
+	return db.Key.from_path('blogs', name)
+
+class Post(db.Model):
+	#author = db.ReferenceProperty(Author)
+	author = db.StringProperty()
+	title = db.StringProperty()
+	content = db.TextProperty()
+	created = db.DateTimeProperty(auto_now_add = True)
+
+	def render(self):
+		self._render_text = self.content.replace('\n', '<br>')
+		return render_str("post.html", p = self)
+
 class MainPage(Handler):
-    def get(self):
-	posts = db.GqlQuery("SELECT * FROM Post "
-			    "ORDER BY created DESC ")
-	self.render("index.html", posts = posts)
+	def get(self):
+		#posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC")
+		posts = Post.all().order('-created')
+		self.render("index.html", posts = posts)
 
-class NewPostHandler(Handler):
-    def get(self):
-	self.render("newpost.html")
+class PostPage(Handler):
+	def get(self, post_id):
+		key = db.Key.from_path("Post", int(post_id), parent=blog_key())
+		post = db.get(key)
+		self.render("permalink.html", post = post)
 
-    def post(self):
-	author = self.request.get("author")
-	title = self.request.get("title")
-	content = self.request.get("content")
-	Post(author = author, title = title, content = content).put()
-	self.redirect("/")
+class NewPost(Handler):
+	def get(self):
+		self.render("newpost.html")
+
+	def post(self):
+		author = self.request.get("author")
+		title = self.request.get("title")
+		content = self.request.get("content")
+		p = Post(parent = blog_key(), author = author, title = title, content = content)
+		p.put()
+		self.redirect("/%s" % str(p.key().id()))
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage), ('/NewPost', NewPostHandler)
+	('/', MainPage), ('/NewPost', NewPost), ('/([0-9]+)', PostPage)
 ], debug=True)
